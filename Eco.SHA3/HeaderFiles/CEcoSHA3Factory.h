09ad1fc8e8c39ec43a45299461364287a1fffa63
Vladimir <Vladimir@vbl.local>
2023-10-31 22:50:36 +0300

    Добавлен проект SHA3

---
+++ Eco.SHA3/HeaderFiles/CEcoSHA3Factory.h
@@ -0,0 +1,42 @@
+﻿/*
+ * <кодировка символов>
+ *   Cyrillic (UTF-8 with signature) - Codepage 65001
+ * </кодировка символов>
+ *
+ * <сводка>
+ *   CEcoSHA3Factory
+ * </сводка>
+ *
+ * <описание>
+ *   Данный заголовок описывает реализацию фабрики для компонента
+ * </описание>
+ *
+ * <автор>
+ *   Copyright (c) 2018 Vladimir Bashev. All rights reserved.
+ * </автор>
+ *
+ */
+
+#ifndef __C_ECOSHA3_FACTORY_H__
+#define __C_ECOSHA3_FACTORY_H__
+
+#include "IEcoSystem1.h"
+
+typedef struct CEcoSHA3_6EFC7840Factory {
+
+    /* Таблица функций интерфейса IEcoComponentFactory */
+    IEcoComponentFactoryVTbl* m_pVTblICF;
+
+    /* Счетчик ссылок */
+    uint32_t m_cRef;
+
+    /* Данные компонентов для фабрики */
+    CreateInstance m_pInstance;
+    InitInstance m_pInitInstance;
+    char_t m_Name[64];
+    char_t m_Version[16];
+    char_t m_Manufacturer[64];
+
+} CEcoSHA3_6EFC7840Factory;
+
+#endif /* __C_ECOSHA3_FACTORY_H__ */
---
+++ Eco.SHA3/SharedFiles/IdEcoSHA3.h
@@ -0,0 +1,42 @@
+﻿/*
+ * <кодировка символов>
+ *   Cyrillic (UTF-8 with signature) - Codepage 65001
+ * </кодировка символов>
+ *
+ * <сводка>
+ *   IdEcoSHA3
+ * </сводка>
+ *
+ * <описание>
+ *   Данный заголовок описывает интерфейс IdEcoSHA3
+ * </описание>
+ *
+ * <ссылка>
+ *
+ * </ссылка>
+ *
+ * <автор>
+ *   Copyright (c) 2018 Vladimir Bashev. All rights reserved.
+ * </автор>
+ *
+ */
+
+#ifndef __ID_ECOSHA3_H__
+#define __ID_ECOSHA3_H__
+
+#include "IEcoBase1.h"
+#include "IEcoSHA3.h"
+
+/* EcoSHA3 CID = {DACA298B-55FA-450D-8B38-C9B16EFC7840} */
+#ifndef __CID_EcoSHA3
+static const UGUID CID_EcoSHA3 = {0x01, 0x10, {0xDA, 0xCA, 0x29, 0x8B, 0x55, 0xFA, 0x45, 0x0D, 0x8B, 0x38, 0xC9, 0xB1, 0x6E, 0xFC, 0x78, 0x40}};
+#endif /* __CID_EcoSHA3 */
+
+/* Фабрика компонента для динамической и статической компановки */
+#ifdef ECO_DLL
+ECO_EXPORT IEcoComponentFactory* ECOCALLMETHOD GetIEcoComponentFactoryPtr();
+#elif ECO_LIB
+extern IEcoComponentFactory* GetIEcoComponentFactoryPtr_DACA298B55FA450D8B38C9B16EFC7840;
+#endif
+
+#endif /* __ID_ECOSHA3_H__ */
